1a635f7bbfac32f617bb373d71901a071a40ff2c
Gabriel Urena <54905586+GabeUrena@users.noreply.github.com>
2023-10-19 12:51:50 -0400

    Added day1 progress.
    Learned print and how to write basic variables.

---
+++ day1.py
@@ -0,0 +1,11 @@
+print("Hello World")
+
+age = 22
+print(age)
+
+age = 22
+price = 41.93
+first_name = "Gabriel"
+is_online = True
+print("The person's name is", first_name, "and they are", age, "years old. Their online status is", is_online, "and they have to pay $", price,".")
+print("This is the price and age combined", age + price)
