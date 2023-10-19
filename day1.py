64ef6be39c2f7ddcbd39a8fe928fa2d685c80344
Gabriel Urena <54905586+GabeUrena@users.noreply.github.com>
2023-10-19 16:03:52 -0400

    Adding updated file
    Includes working with input and type conversion (int)

--- day1.py
+++ day1.py
@@ -9,3 +9,11 @@ first_name = "Gabriel"
 is_online = True
 print("The person's name is", first_name, "and they are", age, "years old. Their online status is", is_online, "and they have to pay $", price,".")
 print("This is the price and age combined", age + price)
+
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+
+print("Hello " + first_name + " " + last_name + " it's nice to meet you.")
+birth_year = input("What year were you born?")
+age = 2023 - int(birth_year)
+print("you must be", age , "years old!")
