332181add4bee6ebd0a49135483786757d1e89fe
Gabriel Urena <54905586+GabeUrena@users.noreply.github.com>
2023-10-23 12:07:59 -0400

    Uploading progress for day 2

---
+++ day2.py
@@ -0,0 +1,7 @@
+print("This is day two of learning Python with Java Experience.")
+
+num1 = input("This is an sum calculator, what is your first number?")
+num2 = input("What's your second number?")
+sum = float(num1) + float(num2)
+
+print("Your Sum:" + str(sum))
