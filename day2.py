23b55aa90b8ec94d283484edec4c50d3dd0d0135
Gabriel Urena <54905586+GabeUrena@users.noreply.github.com>
2023-10-23 17:57:30 -0400

    updating progress

--- day2.py
+++ day2.py
@@ -1,7 +1,13 @@
 print("This is day two of learning Python with Java Experience.")
 
-num1 = input("This is an sum calculator, what is your first number?")
-num2 = input("What's your second number?")
-sum = float(num1) + float(num2)
 
-print("Your Sum:" + str(sum))
+"""num1 = float(input("This is an sum calculator, what is your first number?"))
+num2 = float(input("What's your second number?"))
+sum = num1 + num2
+print("Your Sum: " + str(sum))
+"""
+
+
+course = 'Python tutorial'
+print(course.upper())
+print(course)
